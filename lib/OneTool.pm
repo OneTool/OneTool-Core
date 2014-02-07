e94d1a457792673379fc0ca718abf70835d57d04
Sebastien Thebert <sebthebert@gmail.com>
2014-02-07 23:58:38 +0100

    Main & Configuration modules added

---
+++ lib/OneTool.pm
@@ -0,0 +1,21 @@
+
+=head1 NAME
+
+OneTool - OneTool Main Module
+ 
+=cut
+
+package OneTool;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.1';
+
+1;
+
+=head1 AUTHOR
+
+Sebastien Thebert <contact@onetool.pm>
+
+=cut
---
+++ lib/OneTool/Configuration.pm
@@ -0,0 +1,54 @@
+
+=head1 NAME
+
+OneTool::Configuration - OneTool Configuration module
+
+=cut
+
+package OneTool::Configuration;
+
+use strict;
+use warnings;
+
+use FindBin;
+use File::Slurp;
+use JSON;
+
+my $DIR_CONFIG = "$FindBin::Bin/../conf";
+
+=head1 FUNCTIONS
+
+=head2 Get($param)
+
+Gets configuration from file $param->{file} or for module $param->{module}
+
+=cut
+
+sub Get
+{
+    my $param = shift;
+
+    my $file = (
+        defined $param->{module}
+        ? "$DIR_CONFIG/$param->{module}.conf"
+        : (defined $param->{file} ? $param->{file} : undef)
+    );
+
+    if ((defined $file) && (-r $file))
+    {
+        my $json_str = read_file($file);
+        my $conf     = from_json($json_str);
+
+        return ($conf);
+    }
+
+    return (undef);
+}
+
+1;
+
+=head1 AUTHOR
+
+Sebastien Thebert <contact@onetool.pm>
+
+=cut
